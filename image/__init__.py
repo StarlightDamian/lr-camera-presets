8fed06f2bda6387a3f3472198dbad4ed3c8b1916
zengyuwei <34413764+fengzhisuiyi@users.noreply.github.com>
2024-06-05 21:49:16 +0800

    update

---
+++ image/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 16:04:15 2023
+
+@author: awei
+"""
+import os
+
+# 获取当前脚本所在目录的路径
+path = os.path.dirname(os.path.abspath(__file__))
+    
+print(f"The path to the directory where the current script is located: {path}")
+
+
+    
+    
